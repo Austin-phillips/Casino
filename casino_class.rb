ad28c7eaaa1ea5ddbf6fd867089427eeffa073e8
Austin Phillips <phillips.austin51@gmail.com>
2017-11-13 09:53:09 -0700

    Added code to casino

--- casino_class.rb
+++ casino_class.rb
@@ -1,1 +1,13 @@
 # Game ideas: Wheel of Fortune, Slot Machine, Hi/low,
+
+require 'pry'
+
+class Casino
+  def initialize 
+    puts "Welcome to the ruby casino"
+    puts "What is your name player?"
+    name = gets.strip
+  end
+end
+
+Casino.new
