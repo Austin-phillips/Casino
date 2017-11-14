64bfb84c2ed18ced2bf0a420758b2467024b0dfd
cgregs32 <cgregs32@gmail.com>
2017-11-13 21:35:34 -0700

    little things

--- player.rb
+++ player.rb
@@ -6,13 +6,13 @@ class Player
   attr_accessor :name, :age, :wallet
 
   def initialize
-    puts "What is your name?"
+    puts "What is your name?".yellow
     @name = gets.strip
     @name = name
-    puts "What is your age?"
+    puts "What is your age?".light_green
     @age = gets.to_i
     age_verification
-    puts "How much money are you playing with?"
+    puts "How much money are you playing with?".light_red
     amount = gets.to_f
     @wallet = Wallet.new(amount)
   end
